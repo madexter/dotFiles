0bf4c5308c7ba6754408c3ef5f099f62fdf9b7c2
madexter <53910225+madexter@users.noreply.github.com>
2019-09-02 11:39:56 -0300

    Created parseurl.sh
    Script which parse URL's and Returns the IP Address

---
+++ scripts/parseurl.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+usage() {
+    echo "URL Analyzer"
+    echo "./parseurl.sh URL1 URL2 ..."
+}
+
+echo "|======================================|"
+echo "| -> Automate script to parse URL's <- |"
+echo "|======================================|"
+
+if [ -z $1 ]; then
+    usage
+else
+    echo "[+] You give $# URL(s)!"
+    echo "[+] Analyzing..."
+    
+    for i in "$@"
+    do
+        wget -qO - $i | grep href | cut -d "/" -f3 | grep "\." | cut -d '"' -f1 | 
+            grep -v "<li" | grep -v "top.location.href" | grep -v "return" | 
+            grep -v "javascript" | sort -u > lista_url_"$i".txt
+    done
+
+    for url in $(cat lista_url_*); do host $url; done | grep "has address" 
+fi
